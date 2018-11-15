6f53f6b9ff7176d6804eb62a72e6272608a19f8e
Preethi Krishnamoorthy Ramesh Kumar <pinki@Preethis-MacBook-Air.local>
2018-11-15 10:34:31 -0600

    Intrinsic value

--- extract_points.py
+++ extract_points.py
@@ -12,26 +12,36 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
+concat_array = []
 
 images = glob.glob('*.jpg')
 
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
+    print(len(gray.shape))
     # Find the chess board corners
     ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
-
+        # print("Object Point")
+        # print(len(objpoints))
         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         imgpoints.append(corners2)
-        print(corners2.shape)
-
+        # print("Image Point")
+        # print(len(imgpoints))
+        # print(type(objpoints))
+        # print(imgpoints)
+        # concat_array.append(objp + corners2)
+        # print(corners2.shape)
+        # print(concat_array)
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+        print("Camera calibrated")
+        print(mtx)
         cv2.imshow('img',img)
         cv2.waitKey(500)
 
